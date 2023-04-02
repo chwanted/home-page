6f330b6b982a76164d844297c91fc1f8bcca42bf
1024HY <2727968152@qq.com>
2023-04-02 22:54:40 +0800

    地图调整

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -18,7 +18,8 @@ export default {
   name: 'LeafletMap',
   data() {
     return {
-      map: null
+      map: null,
+      icon: null
     }
   },
   mounted() {
@@ -36,11 +37,31 @@ export default {
         attributionControl: true, // 版权控件
         zoomControl: true //缩放控件
       });
+      // 写不来了 宝~
+      let myIcon = L.icon({
+        iconUrl: 'my-icon.png',
+        iconSize: [38, 95],
+        iconAnchor: [22, 94],
+        popupAnchor: [-3, -76],
+        shadowUrl: 'src/assets/images/location.png',
+        shadowSize: [68, 95],
+        shadowAnchor: [22, 94]
+      });
+      this.icon = myIcon; // data上需要挂载
+
       this.map = map; // data上需要挂载
       console.log(map)
+
+      // 加载底图
       L.tileLayer(
           "https://wprd04.is.autonavi.com/appmaptile?lang=zh_cn&size=1&style=7&x={x}&y={y}&z={z}"
-      ).addTo(map) // 加载底图
+      ).addTo(map)
+
+      // 加载定位图标
+      const longitude = 29.32132 // 经度
+      const latitude = 106.33 // 纬度
+      L.marker([longitude, latitude], {icon: myIcon}).addTo(map);
+
     }
   }
 }
@@ -50,7 +71,7 @@ export default {
   /*width: 800px;*/
   /*height: 500px;*/
   /*margin: 50px auto;*/
-  position:relative;
+  position: relative;
   top: 341px;
   left: 0;
   width: 100%;
